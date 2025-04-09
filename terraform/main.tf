c58ce4f5f49eaceb889299eb8a12dd86dae707b9
Zoheb Amin <zoheb1amin@gmail.com>
2025-04-09 15:12:11 -0500

    Terraform to AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "flask_app" {
+  ami           = "ami-0c94855ba95c71c99" # Update with your preferred AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "FlaskAppInstance"
+  }
+}
 
