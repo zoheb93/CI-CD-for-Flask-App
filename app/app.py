af1eaab0f51b43238307a736f264ab69e88748db
Zoheb Amin <zoheb1amin@gmail.com>
2025-04-09 15:11:50 -0500

    Python app in Flask to print hello world

--- app/app.py
+++ app/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify(message="Hello from CI/CD!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
