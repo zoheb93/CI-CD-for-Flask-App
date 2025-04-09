c919f218eaa5d20d4b3e57eab8e3136703a0e46d
Zoheb Amin <zoheb1amin@gmail.com>
2025-04-09 15:11:03 -0500

    Setup testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,8 @@
+from app import app
+
+def test_root():
+    with app.test_client() as client:
+        res = client.get("/")
+        assert res.status_code == 200
+        assert res.json == {"message": "Hello from CI/CD!"}
 
